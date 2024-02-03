b47a8d8b4656840180ade3fac9007e24f9e22318
Apostolova8 <152811140+Apostolova8@users.noreply.github.com>
2024-02-03 20:05:46 +0200

    Add files via upload

---
+++ Arrays_/FileName.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	//1:
+	//	int temperature[10];
+	//	for (int i = 0; i < 10; i++) {
+	//		cout << "Enter temperature: " << endl;
+	//			cin >> temperature[i];
+	//	}
+	//
+	//	float sum = 0;
+	//	for (int i = 0; i < 10; i++) {
+	//		sum = sum + temperature[i];
+	//	}
+	//
+	//		cout << "Sum is " << sum << endl;
+	//		cout << "Average is " << sum / 10 << endl;
+	//	
+	//
+	//	for (int i = 0; i < 10; i++) {
+	//		cout << i + 1 << " => " << temperature[i] << endl;
+	//	}
+	//}
+
+
+	//2: 
+	//	int temperature[10];
+	//	for (int i = 0; i < 10; i++) {
+	//		cout << "Enter temperature: " << endl;
+	//			cin >> temperature[i];
+	//	}
+	//
+	//	int count = 0;
+	//	for (int i = 0; i < 10; i++) {
+	//		if (temperature[i] > 0) {
+	//			count++;
+	//		}
+	//	}
+	//
+	//	cout << "Positive temps: " << count << endl;
+	//
+	//	for (int i = 0; i < 10; i++) {
+	//		cout << i + 1 << " => " << temperature[i] << endl;
+	//	}
+	//}
+
+
+	//3:
+	/*
+	int temperature[30] = { 12,-5,-3,-2,-1,23,12,16,25,-3,-4,0,12,15,17,23,-5,-6,-4,-2,0,12,12,24,14,15,15,16,14.16,13 };
+	int count = 0;
+	for (int i = 0; i < 30; i++) {
+		if (temperature[i] >= 0) {
+			count++;
+		}
+	}
+	for (int i : temperature) {
+		cout << i << endl;
+	}
+	cout << "Positive temps: " << count << endl;
+}
+*/
