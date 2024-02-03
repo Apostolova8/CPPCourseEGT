0a79eef9345a9c2c8f4b305fd79756101e49e183
Apostolova8 <152811140+Apostolova8@users.noreply.github.com>
2024-02-03 20:17:54 +0200

    Add files via upload

---
+++ Positive numbers_Homework/FileName.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int n;
+	cout << "Enter array size: " << endl;
+	cin >> n;
+	int i;
+	int A[i];
+	int positiveNumber;
+
+	for (int i = 0; i < 0; ++i) {
+		cin >> A[i];
+	}
+	positiveNumber = A[0];
+	for (int i = 0; i < n; ++i) {
+		if (positiveNumber > 0)
+			cout << positiveNumber;
+
+	}
+}
