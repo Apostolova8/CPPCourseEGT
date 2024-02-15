2242c1d4d57ac217b4de131128ba996c454d485b
Apostolova8 <152811140+Apostolova8@users.noreply.github.com>
2024-02-15 13:16:58 +0200

    Update FileName.cpp

--- LeapYear_Homework/FileName.cpp
+++ LeapYear_Homework/FileName.cpp
@@ -2,26 +2,26 @@
 using namespace std;
 
 int main() {
-	int date;
-	bool leapYear;
-	int months[] = { 0,0,0,0,0,0,0,0,0,0,0,0 };
-	int firstDay;
-	cin >> firstDay >> leapYear;
-	int monthSize[] = { 31, 28, 31, 30, 31, 30, 31, 30, 31, 31, 30, 31 };
-	monthSize[1] += leapYear;
+    int date;
+    bool leapYear;
+    int months[] = { 0,0,0,0,0,0,0,0,0,0,0,0 };
+    int firstDay;
+    cin >> firstDay >> leapYear;
+    int monthSize[] = { 31, 28, 31, 30, 31, 30, 31, 30, 31, 31, 30, 31 };
+    monthSize[1] += leapYear;
 
-	for (int i = 0; i < 12; ++i) {
-		if ((firstDay + 12) % 7 == 4) {
-			months[i] += 1;
-			firstDay = (firstDay + monthSize[i]) % 7;
-		}
-	}
+    for (int i = 0; i < 12; ++i) {
+        if ((firstDay + 12) % 7 == 4) {
+            months[i] += 1;
+        }
+        firstDay = (firstDay + monthSize[i]) % 7;
+    }
 
-	for (int i = 0; i < 12; ++i) {
-		cout << months[i] << " ";
-	}
+    for (int i = 0; i < 12; ++i) {
+        cout << months[i] << " ";
+    }
 
-	cout << endl;
+    cout << endl;
 
-	return 0;
+    return 0;
 }
